3f9f29965d285ca1a2bcbdcb73df0cf9ba09e567
ykarimi <ykarimi.pro@gmail.com>
2019-09-05 18:24:40 +0430

    data is done

---
+++ app/src/main/java/com/example/weather/models/Current.java
@@ -0,0 +1,5 @@
+package com.example.weather.models;
+
+public class CurrentWeather {
+
+}
---
+++ app/src/main/java/com/example/weather/models/CurrentMain.java
@@ -0,0 +1,7 @@
+package com.example.weather.models;
+
+public class CurrentWeather {
+    public String icon;
+    public String main;
+    public String description;
+}
